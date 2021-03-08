3df68bb1af66ebe3fa0cc5330bb812c367ff10c8
sirzes02 <sirzes02@gmail.com>
2021-03-08 05:10:08 -0500

    --fix - "config"

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = (process.env.NODE_ENV || "production") === "production";
-
-module.exports = {
-  exportPathMap: () => ({
-    "/": { page: "/" },
-  }),
-  assetPrefix: isProd ? "/sirzes02.github.io" : "",
-};
