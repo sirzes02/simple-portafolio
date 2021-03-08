4e35c02e1c65cbebbaef4b2aa551661265332c75
sirzes02 <sirzes02@gmail.com>
2021-03-08 02:34:04 -0500

    add - "test seccion - incomplete"

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
+import { skills, experiences, tests } from "../profile";
 
 const Index = () => (
   <Layout>
@@ -28,6 +30,85 @@ const Index = () => (
         </div>
       </div>
     </header>
+
+    <div className="row py-2">
+      <div className="col-md-4">
+        <div className="card bg-ligth">
+          <div className="card-body">
+            <h1>Skills</h1>
+            {skills.map(({ skill, percentage }, i) => (
+              <div className="py-3" key={i}>
+                <h5>{skill}</h5>
+                <div className="progress">
+                  <div
+                    className="progress-bar"
+                    role="progress"
+                    aria-valuenow="50"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: `${percentage}%` }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="col-md-8">
+        <div className="card bg-ligth">
+          <div className="card-body">
+            <h1>Experience</h1>
+            <ul>
+              {experiences.map(({ title, description, from, to }, i) => (
+                <li key={i}>
+                  <h3>{title}</h3>
+                  <h5>
+                    {from}-{to}
+                  </h5>
+                  <p>{description}</p>
+                </li>
+              ))}
+            </ul>
+            <Link href="/experiences">
+              <a className="btn btn-ligth disabled">Khow more</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-dark">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="text-center text-light">Programming Tests</h1>
+            </div>
+            {tests.map(({ name, description, image, link }, i) => (
+              <div className="col-md-4 p-2" key={i}>
+                <div className="card h-100">
+                  <div className="overflow">
+                    <img
+                      src={`/${image}`}
+                      alt="test image"
+                      className="img-fluid border border-dark"
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h3>{name}</h3>
+                    <p>{description}</p>
+                    <a href={link} target="_blank">
+                      Know more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
---
+++ profile.js
@@ -0,0 +1,63 @@
+export const skills = [
+  {
+    skill: "React",
+    percentage: 75,
+  },
+  {
+    skill: "Laravel",
+    percentage: 70,
+  },
+  {
+    skill: "Mobile Development",
+    percentage: 70,
+  },
+  {
+    skill: "Backend",
+    percentage: 70,
+  },
+  {
+    skill: "Frontend",
+    percentage: 65,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Junior Developer at USC",
+    description:
+      "I worked for the Santiago de Cali University as a Junior Developer, developing a web application focused on Virtual Reality, using tools such as NodeJS, WebPack and ThreeJS, in turn, using the Moodle platform as a deployment method.",
+    from: 2019,
+    to: 2021,
+  },
+  {
+    title: "Programming monitor teacher at USC",
+    description:
+      "I was a monitor of algorithmic and programming I and II as well as data structure, during this period of time, I was in charge of strengthening the logical knowledge of the students in programming languages ​​such as Java, C and C ++, in turn, teaching pseudocode using of the Pseint application.",
+    from: 2017,
+    to: "Now",
+  },
+];
+
+export const tests = [
+  {
+    name: "GNS table",
+    description:
+      "Technical test developed in React, making use of the Bootstrap and React-tables libraries, handling the request level of an API.",
+    image: "test-1.png",
+    link: "https://github.com/sirzes02/gns_project",
+  },
+  {
+    name: "Mini Twitter",
+    description:
+      "Technical test developed in React Native, using Firebase as a backend simulation, where a clone of the basic functioning of twitter was programmed.",
+    image: "test-2.jpg",
+    link: "https://github.com/sirzes02/mini_twitter",
+  },
+  {
+    name: "Purchase of products",
+    description:
+      "Technical test developed in Laravel, using Mysql as the main database, where a product purchase web page was programmed, a page that had administrator-user roles management.",
+    image: "test-3.png",
+    link: "https://github.com/sirzes02/aplicacion_compras",
+  },
+];
